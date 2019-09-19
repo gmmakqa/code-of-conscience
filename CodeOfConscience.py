f8bbc3354b26227d72f2fc3e5c662461d0318dd1
Gerard Mason <gerard.mason@akqa.com>
2019-09-19 15:09:11 +1000

    Added Raspberry Pi / SixFab IoT test code Added shapefiles for test location

--- CodeOfConscience.py
+++ CodeOfConscience.py
@@ -1,13 +1,56 @@
-import geopandas as gpd
-from shapely.geometry import Point, Polygon
-from joblib import Parallel, delayed
 import multiprocessing
+import time
+from decimal import *
 from functools import partial
 
-machineLocation = Point(175.8704514016789, -37.73493520423763)
+import geopandas as gpd
+from cellulariot import cellulariot
+from joblib import Parallel, delayed
+from shapely.geometry import Point, Polygon
 
-shapefile = "WDPA_Aug2019-shapefile-polygons.shp"
+import RPi.GPIO as GPIO
+
+# Information
+num_mobile = "XXXXXXXXXX"
+id_machine = "CoC1"
+allow_SMS = True
+
+# Initializing Sixfab CellularIoT App. Shield
+node = cellulariot.CellularIoTApp()
+node.setupGPIO()
+node.disable()
+time.sleep(1)
+node.enable()
+time.sleep(1)
+node.powerUp()
+
+# LTE connection Check
+node.getResponse("RDY")
+node.sendATComm("ATE1", "OK\r\n")
+time.sleep(0.5)
+
+# Connecting GPS
+node.turnOnGNSS()
+time.sleep(60)
+fixed = node.getFixedLocation()
+time.sleep(1)
+node.sendSMS(num_mobile, "GPS location has been fixed.")
+print("GPS location has been fixed. Reading the current position../")
+time.sleep(1)
+longitude = node.getLongitude()
+time.sleep(1)
+latitude = node.getLatitude()
+time.sleep(1)
+node.sendSMS(num_mobile, "The current location - Latitude: {}, Longtitude: {}".format(latitude, longitude))
+print("The current location - Latitude: {}, Longtitude: {}".format(latitude, longitude))
+
+
+# Loading protected area map file
+print("Loading protected area...")
+shapefile = "organ-pipes-shapefile-polygons.shp"
 df = gpd.read_file(shapefile)
+machineLocation = Point(longitude, latitude)
+print("Ready to check...")
 
 
 def processInput(i, point):
@@ -20,9 +63,30 @@ def isInProtectedArea(machineLocation):
     with multiprocessing.Pool(num_cores) as p:
         result = p.map(process, range(len(df)))
     if sum(result):
-        print("The machine is inside a protected area")
+        print(" ")
+        print("[!] The machine is inside a protected area")
+        if allow_SMS:
+            node.sendSMS(num_mobile, "The machine: " + id_machine + " is inside a protected area")
+        allow_SMS = False
+        node.turnOnRelay()
     else:
-        print("The machine is not inside a protected area")
+        print(" ")
+        print("[√] The machine is not inside a protected area")
+        node.turnOffRelay()
+        allow_SMS = True
 
 
-isInProtectedArea(machineLocation)
+print("Monitoring in progress! Press CTRL+C to exit")
+try:
+    while 1:
+        time.sleep(0.5)
+        longitude = node.getLongitude()
+        time.sleep(0.5)
+        latitude = node.getLatitude()
+        machineLocation = Point(longitude, latitude)
+        if (longitude is not 0) & (latitude is not 0):
+            isInProtectedArea(machineLocation)
+        else:
+            print('GPS data is missing')
+except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup()  # cleanup all GPIO
